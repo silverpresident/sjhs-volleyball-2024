a2bffdaa8cdff44383e8c61f9a4fd638a394fc80
silverpresident <4566006+silverpresident@users.noreply.github.com>
2025-01-04 07:52:50 +0000

    fixed seeding

--- src/VolleyballRallyManager.Lib/Configuration/DatabaseInitialization.cs
+++ src/VolleyballRallyManager.Lib/Configuration/DatabaseInitialization.cs
@@ -51,21 +51,60 @@ namespace VolleyballRallyManager.Lib.Configuration
             }
 
             // Create default admin user if it doesn't exist
-            var adminUser = await userManager.FindByNameAsync("admin");
+            var adminUserDefault = new IdentityUser { UserName = "admin", Email = "admin@example.com" };
+            var adminUser = await userManager.FindByNameAsync(adminUserDefault.UserName);
             if (adminUser == null)
             {
-                adminUser = new IdentityUser { UserName = "admin", Email = "admin@example.com" };
-                await userManager.CreateAsync(adminUser, "admin123");
-
-                // Assign the Admin role to the user
-                await userManager.AddToRoleAsync(adminUser, "Admin");
+                await userManager.CreateAsync(adminUserDefault, "admin123");
             }
+            else
+            {
+                await userManager.SetEmailAsync(adminUserDefault, adminUserDefault.Email);
+                await userManager.AddPasswordAsync(adminUserDefault, "admin123");
+            }
+
+            // Assign the Admin role to the user
+            await userManager.AddToRoleAsync(adminUserDefault, "Admin");
         }
 
-        private static async Task SeedInitialDataAsync(ApplicationDbContext dbContext)
+        private static async Task<List<Division>> SeedDivisionsAsync(ApplicationDbContext dbContext)
         {
+            var divisions = new List<Division>();
+            // Add sample divisions
+            var defaultDivisions = new[]
+            {
+                new Division {Name = "BOYS"},
+                new Division {Name = "GIRLS"}
+            };
+            foreach (var division in defaultDivisions)
+            {
+                if (string.IsNullOrEmpty(division.Name))
+                {
+                    division.Name = "Unknown";
+                }
+                if (dbContext.Divisions.Any(d => d.Name == division.Name))
+                {
+                    divisions.Add(dbContext.Divisions.First(d => d.Name == division.Name));
+                }
+                else
+                {
+                    division.UpdatedAt = DateTime.Now;
+                    division.CreatedAt = DateTime.Now;
+                    division.UpdatedBy = "System";
+                    division.CreatedBy = "System";
+                    dbContext.Divisions.Add(division);
+                    divisions.Add(division);
+                }
+            }
+            await dbContext.SaveChangesAsync();
+            return divisions;
+        }
+
+        private static async Task<List<Round>> SeedRoundsAsync(ApplicationDbContext dbContext)
+        {
+            var rounds = new List<Round>();
             // Add sample rounds
-            var rounds = new[]
+            var defaultRounds = new[]
             {
                 new Round { Id = Guid.NewGuid(), Name = "Round 1", Sequence = 1 },
                 new Round { Id = Guid.NewGuid(), Name = "Round 2", Sequence = 2 },
@@ -74,59 +113,74 @@ namespace VolleyballRallyManager.Lib.Configuration
                 new Round { Id = Guid.NewGuid(), Name = "Semi Finals", Sequence = 5 },
                 new Round { Id = Guid.NewGuid(), Name = "Finals", Sequence = 6 }
             };
-            // Add sample divisions
-            var divisions = new[]
+            foreach (var round in defaultRounds)
             {
-                new Division { Id = Guid.NewGuid(), Name = "BOYS"},
-                new Division { Id = Guid.NewGuid(), Name = "GIRLS"}
-            };
 
-            foreach (var division in divisions)
-            {
-                if (string.IsNullOrEmpty(division.Name))
+                if (dbContext.Rounds.Any(d => d.Name == round.Name))
                 {
-                    division.Name = "Unknown";
+                    rounds.Add(dbContext.Rounds.First(d => d.Name == round.Name));
+                }
+                else
+                {
+                    round.UpdatedAt = DateTime.Now;
+                    round.CreatedAt = DateTime.Now;
+                    round.UpdatedBy = "System";
+                    round.CreatedBy = "System";
+                    dbContext.Rounds.Add(round);
+                    rounds.Add(round);
                 }
             }
-
-            await dbContext.Rounds.AddRangeAsync(rounds);
-            await dbContext.Divisions.AddRangeAsync(divisions);
             await dbContext.SaveChangesAsync();
+            return rounds;
+        }
+        private static async Task SeedInitialDataAsync(ApplicationDbContext dbContext)
+        {
+            // Add sample divisions
+            var divisions = await SeedDivisionsAsync(dbContext);
+            var rounds = await SeedRoundsAsync(dbContext);
+
 
             // Add sample teams
             var teams = new[]
             {
-                new Team { 
-                    Id = Guid.NewGuid(), 
-                    Name = "Team A", 
+                new Team {
+                    Id = Guid.NewGuid(),
+                    Name = "Team A",
                     School = "School A",
                     Color = "#FF0000",
                     Division = divisions[0]
                 },
-                new Team { 
-                    Id = Guid.NewGuid(), 
-                    Name = "Team B", 
+                new Team {
+                    Id = Guid.NewGuid(),
+                    Name = "Team B",
                     School = "School B",
                     Color = "#00FF00",
                     Division = divisions[0]
                 },
-                new Team { 
-                    Id = Guid.NewGuid(), 
-                    Name = "Team C", 
+                new Team {
+                    Id = Guid.NewGuid(),
+                    Name = "Team C",
                     School = "School C",
                     Color = "#0000FF",
                     Division = divisions[1]
                 },
-                new Team { 
-                    Id = Guid.NewGuid(), 
-                    Name = "Team D", 
+                new Team {
+                    Id = Guid.NewGuid(),
+                    Name = "Team D",
                     School = "School D",
                     Color = "#FFFF00",
                     Division = divisions[1]
                 }
             };
-
-            await dbContext.Teams.AddRangeAsync(teams);
+            foreach (var team in teams)
+            {
+                team.UpdatedAt = DateTime.Now;
+                team.CreatedAt = DateTime.Now;
+                team.UpdatedBy = "System";
+                team.CreatedBy = "System";
+            }
+            if (await dbContext.Teams.AnyAsync())
+                await dbContext.Teams.AddRangeAsync(teams);
             await dbContext.SaveChangesAsync();
 
             // Add sample announcement
@@ -136,7 +190,10 @@ namespace VolleyballRallyManager.Lib.Configuration
                 Content = "Welcome to ST JAGO VOLLEYBALL RALLY!",
                 Priority = AnnouncementPriority.Info,
                 IsVisible = true,
-                CreatedAt = DateTime.UtcNow
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow,
+                CreatedBy = "System",
+                UpdatedBy = "System"
             };
 
             await dbContext.Announcements.AddAsync(announcement);
