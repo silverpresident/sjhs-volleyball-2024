b087b9bad3f96e8d0e6b5baa57e4739c9bfea537
silverpresident <4566006+silverpresident@users.noreply.github.com>
2025-11-23 17:21:45 +0000

    some updates

---
+++ database/cleanup-usertables.sql
@@ -0,0 +1,31 @@
+-- Drop all user tables
+USE stjago-volleyball-demo;
+GO
+
+IF OBJECT_ID('dbo.AspNetUsers', 'U') IS NOT NULL
+    DROP TABLE dbo.AspNetUsers;
+GO
+
+IF OBJECT_ID('dbo.AspNetRoles', 'U') IS NOT NULL
+    DROP TABLE dbo.AspNetRoles;
+GO
+
+IF OBJECT_ID('dbo.AspNetRoleClaims', 'U') IS NOT NULL
+    DROP TABLE dbo.AspNetRoleClaims;
+GO
+
+IF OBJECT_ID('dbo.AspNetUserClaims', 'U') IS NOT NULL
+    DROP TABLE dbo.AspNetUserClaims;
+GO
+
+IF OBJECT_ID('dbo.AspNetUserLogins', 'U') IS NOT NULL
+    DROP TABLE dbo.AspNetUserLogins;
+GO
+
+IF OBJECT_ID('dbo.AspNetUserRoles', 'U') IS NOT NULL
+    DROP TABLE dbo.AspNetUserRoles;
+GO
+
+IF OBJECT_ID('dbo.AspNetUserTokens', 'U') IS NOT NULL
+    DROP TABLE dbo.AspNetUserTokens;
+GO
--- database/cleanup.sql
+++ database/cleanup.sql
@@ -38,31 +38,3 @@ GO
 IF OBJECT_ID('dbo.Announcements', 'U') IS NOT NULL
     DROP TABLE dbo.Announcements;
 GO
-
-IF OBJECT_ID('dbo.AspNetUsers', 'U') IS NOT NULL
-    DROP TABLE dbo.AspNetUsers;
-GO
-
-IF OBJECT_ID('dbo.AspNetRoles', 'U') IS NOT NULL
-    DROP TABLE dbo.AspNetRoles;
-GO
-
-IF OBJECT_ID('dbo.AspNetRoleClaims', 'U') IS NOT NULL
-    DROP TABLE dbo.AspNetRoleClaims;
-GO
-
-IF OBJECT_ID('dbo.AspNetUserClaims', 'U') IS NOT NULL
-    DROP TABLE dbo.AspNetUserClaims;
-GO
-
-IF OBJECT_ID('dbo.AspNetUserLogins', 'U') IS NOT NULL
-    DROP TABLE dbo.AspNetUserLogins;
-GO
-
-IF OBJECT_ID('dbo.AspNetUserRoles', 'U') IS NOT NULL
-    DROP TABLE dbo.AspNetUserRoles;
-GO
-
-IF OBJECT_ID('dbo.AspNetUserTokens', 'U') IS NOT NULL
-    DROP TABLE dbo.AspNetUserTokens;
-GO
