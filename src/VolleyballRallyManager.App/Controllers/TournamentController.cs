29dee6b4ba95729bc69d70a89ec6db7ae3f26122
silverpresident <4566006+silverpresident@users.noreply.github.com>
2025-12-23 17:22:37 +0000

    Add public tournament details controller

---
+++ src/VolleyballRallyManager.App/Controllers/TournamentController.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Mvc;
+using VolleyballRallyManager.App.Models;
+using VolleyballRallyManager.Lib.Common;
+using VolleyballRallyManager.Lib.Data;
+using VolleyballRallyManager.Lib.Services;
+
+namespace VolleyballRallyManager.App.Controllers;
+
+public class TournamentController : Controller
+{
+    private readonly ApplicationDbContext _context;
+    private readonly ILogger<TournamentController> _logger;
+    private readonly IActiveTournamentService _activeTournamentService;
+    private readonly IBulletinService _bulletinService;
+
+    public TournamentController(ApplicationDbContext context, IActiveTournamentService activeTournamentService, ILogger<TournamentController> logger, IBulletinService bulletinService)
+    {
+        _context = context;
+        _logger = logger;
+        _activeTournamentService = activeTournamentService;
+        _bulletinService = bulletinService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var teamsByDivision = new Dictionary<string, int>();
+        var activeTournament = await _activeTournamentService.GetActiveTournamentAsync();
+        if (activeTournament != null)
+        {
+            var divisions = await _activeTournamentService.GetTournamentDivisionsAsync();
+
+            foreach (var division in divisions)
+            {
+                teamsByDivision[division.Division.Name] = await _activeTournamentService.GetTournamentTeamsCountAsync(division.DivisionId);
+            }
+        }
+
+
+//TODO remove unnecsary things
+        var dashboard = new DashboardViewModel
+        {
+            TotalTeams = await _activeTournamentService.TeamCountAsync(),
+            TotalMatches = await _activeTournamentService.MatchCountAsync(),
+            MatchesInProgress = await _activeTournamentService.MatchCountAsync(MatchState.InProgress),
+            DisputedMatches = await _activeTournamentService.MatchCountAsync(MatchState.Disputed),
+            MatchesFinished = await _activeTournamentService.MatchCountAsync(MatchState.Finished),
+            RecentMatches = await _activeTournamentService.RecentMatchesAsync(12),
+            RecentBulletins = await _bulletinService.GetRecentAsync(5),
+            TeamsByDivision = teamsByDivision,
+            ActiveTournament = activeTournament
+        };
+        if (activeTournament != null)
+        {
+            dashboard.ActiveTournamentName = activeTournament.Name;
+        }
+
+        return View(dashboard);
+    } 
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = System.Diagnostics.Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
